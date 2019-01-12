cee3af4217c8af0fceee99ac3eaa728df583b73b
Chovin Carlson <Chovin@users.noreply.github.com>
2019-01-12 06:43:54 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -6,9 +6,11 @@ class CashRegister
     @total = 0
     @discount = discount
     @items = []
+    @prev_total = 0
   end
   
   def add_item(title, price, quantity=1)
+    @prev_total = @total
     @total += price * quantity
     @items.fill title, @items.size, quantity
   end
@@ -17,6 +19,10 @@ class CashRegister
     return "There is no discount to apply." if @discount.nil?
     @total *= 1-@discount/100.0
     "After the discount, the total comes to $#{@total.round}."
+  end
+  
+  def void_last_transaction
+    @total = @prev_total
   end
   
 end
